9099bb913d1dff3307f439582805c4883abac8d0
Aneta Krajčovicová <anet.krajcovicovie@gmail.com>
2024-05-19 13:56:54 +0200

    changed base in vite.config.js

--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,7 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    
   },
   {
     path: "/success",
@@ -19,6 +20,6 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <RouterProvider  router={router} />
   </React.StrictMode>
 );
